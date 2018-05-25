b69cc769ce99f14a125055ef38fcf0b3ac55e70f
Nguyen Thuong Thong <nguyenthuongthongcm@gmail.com>
2018-05-25 22:05:57 +0700

    Update pod

---
+++ RxMoyaExample/IssueListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RxMoyaExample
+//
+//  Created by ninjaKID on 5/25/18.
+//  Copyright © 2018 ninjaKID. All rights reserved.
+//
+
+import UIKit
+
+class IssueListViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
