72118d45d383af1e1a69a6619b014e88dbf38d76
Nguyen Thuong Thong <nguyenthuongthongcm@gmail.com>
2018-05-25 21:47:33 +0700

    Install lib

--- RxMoyaExample/ViewController.swift
+++ RxMoyaExample/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
